b2057eb001c1e7e2e05b305f9e914def0d834c56
shinbastien <jugipalace@kaist.ac.kr>
2022-10-18 18:50:32 +0900

    Add python solution for 234

---
+++ easy/234_Palindrome_Linked_List/234_Palindrome_Linked_List_Solution.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+
+    def isPalindrome(self, head):
+
+        self.front_pointer = head
+
+        def recursively_check(current_node=head):
+            if current_node is not None:
+                if not recursively_check(current_node.next):
+                    return False
+                if self.front_pointer.val != current_node.val:
+                    return False
+                self.front_pointer = self.front_pointer.next
+            return True
+
+        return recursively_check()
